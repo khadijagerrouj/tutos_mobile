15b581b3eda41e90fe21d7c4d3b031d0bcc8fc20
khadija <khadijagerrouj4@gmail.com>
2026-09-22 16:21:10 +0000

    php

---
+++ Structurer l’application en composants/7_Définir_la_communication(HTTP_&_API)/backend/categories.php
@@ -0,0 +1,11 @@
+<?php
+header('content-type: application/json');
+
+$categories =[
+    ["id"=> 1, "nom"=> "devloppement web"],
+    ["id"=> 2, "nom"=>"design ui/ux"]
+];
+echo json_encode($categories);
+
+
+?>
